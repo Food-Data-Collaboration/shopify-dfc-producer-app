82026a1f058f4783e6fd2b0d7ad29e923d036012
Alex Luker <alex.luker@gmail.com>
2025-03-05 17:02:45 +0000

    fix

--- web/database/variants/variants.js
+++ web/database/variants/variants.js
@@ -2,19 +2,16 @@ import { query } from '../connect.js';
 
 async function getVariants() {
   return (await query('SELECT * FROM fdc_variants order by product_id')).rows;
-  return (await query('SELECT * FROM fdc_variants order by product_id')).rows;
 }
 
 async function getVariantsByProductId(productId) {
   return (
-    await query('SELECT * FROM fdc_variants where product_id = $1', [productId])
     await query('SELECT * FROM fdc_variants where product_id = $1', [productId])
   ).rows;
 }
 
 async function variantCount() {
   return Number(
-    (await query('SELECT count(*) FROM fdc_variants')).rows[0].count
     (await query('SELECT count(*) FROM fdc_variants')).rows[0].count
   );
 }
