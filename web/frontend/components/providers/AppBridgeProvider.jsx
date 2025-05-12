d18a06e9a8149f37859a27834b353291193a48f9
Alex Luker <alex.luker@gmail.com>
2025-05-12 12:52:21 +0100

    more debugging

--- web/frontend/components/providers/AppBridgeProvider.jsx
+++ web/frontend/components/providers/AppBridgeProvider.jsx
@@ -41,6 +41,9 @@ export function AppBridgeProvider({ children }) {
 
     window.__SHOPIFY_DEV_HOST = host;
 
+    console.log('new URLSearchParams(location.search).get(host)', new URLSearchParams(location.search).get('host'));
+    console.log('process.env.SHOPIFY_API_KEY', process.env.SHOPIFY_API_KEY);
+
     return {
       host,
       apiKey: process.env.SHOPIFY_API_KEY,
@@ -51,25 +54,33 @@ export function AppBridgeProvider({ children }) {
   if (!process.env.SHOPIFY_API_KEY || !appBridgeConfig.host) {
     const bannerProps = !process.env.SHOPIFY_API_KEY
       ? {
-          title: 'Missing Shopify API Key',
-          children: (
-            <>
-              Your app is running without the SHOPIFY_API_KEY environment
-              variable. Please ensure that it is set when running or building
-              your React app.
-            </>
-          )
-        }
+        title: 'Missing Shopify API Key',
+        children: (
+          <>
+            Your app is running without the SHOPIFY_API_KEY environment
+            variable. Please ensure that it is set when running or building
+            your React app.
+          </>
+        )
+      }
       : {
-          title: 'Missing host query argument',
-          children: (
-            <>
-              Your app can only load if the URL has a <b>host</b> argument.
-              Please ensure that it is set, or access your app using the
-              Partners Dashboard <b>Test your app</b> feature
-            </>
-          )
-        };
+        title: 'Missing host query argument',
+        children: (
+          <>
+            Your app can only load if the URL has a
+            {' '}
+            <b>host</b>
+            {' '}
+            argument.
+            Please ensure that it is set, or access your app using the
+            Partners Dashboard
+            {' '}
+            <b>Test your app</b>
+            {' '}
+            feature
+          </>
+        )
+      };
 
     return (
       <Page narrowWidth>
