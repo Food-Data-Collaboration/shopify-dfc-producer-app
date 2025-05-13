191d86c097cc2a9746d972ccf3280bb31fde60aa
AhmadSunono <asonouno@gmail.com>
2025-05-13 11:49:11 +0300

    feat: include store front access token in shop details query

--- web/database/shop_registry/shops.js
+++ web/database/shop_registry/shops.js
@@ -1,7 +1,10 @@
 import { query } from '../connect.js';
 
 export const fetchShopDetails = async (shopName) => {
-  const shopDetails = await query('SELECT shop_name, variant_mappings_enabled, setup_completed, orders_feature_enabled, default_product_type FROM shops WHERE shop_name = $1', [shopName]);
+  const shopDetails = await query(
+    'SELECT shop_name, variant_mappings_enabled, setup_completed, orders_feature_enabled, default_product_type, store_front_access_token FROM shops WHERE shop_name = $1',
+    [shopName]
+  );
 
   return shopDetails.rows[0];
 };
