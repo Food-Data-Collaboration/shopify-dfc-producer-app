3f85684345690a5bec966f8a9b1216a8442de479
Alex Luker <alex.luker@gmail.com>
2025-06-02 17:17:15 +0100

    308 311 - upgrade connector and enable all products for fdc on installation when variant mapping disabled

--- web/api-modules/shop/controllers/complete-setup.js
+++ web/api-modules/shop/controllers/complete-setup.js
@@ -1,8 +1,10 @@
 import { query } from '../../../database/connect.js';
+import ProductUseCases from '../../products/use-cases/index.js';
+import { setAllVariantMappingStatuses } from '../../../database/variants/variants.js';
 
 const completeSetup = async (req, res, next) => {
   try {
-    const { shopName } = req;
+    const { shopName, shopifySession } = req;
     const { variantMappingsEnabled, defaultProductType } = req.body;
 
     const result = await query(
@@ -14,6 +16,20 @@ const completeSetup = async (req, res, next) => {
       return res.status(404).json({
         error: 'Shop not found'
       });
+    }
+
+    if (!variantMappingsEnabled) {
+      const products = await ProductUseCases.getProducts({
+        session: shopifySession,
+        shopName
+      });
+
+      await setAllVariantMappingStatuses(
+        products.flatMap((product) =>
+          product.variants.map((variant) => ({ productId: product.id, variantId: variant.id }))),
+        true,
+        shopName
+      );
     }
 
     return res.status(200).json({
