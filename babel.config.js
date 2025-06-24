dd4eebe35184203fdf8165d4ea1e73a1fe73a910
Alex Luker <alex.luker@gmail.com>
2025-06-24 13:45:56 +0100

    remove legacy babel plugin that was stopping modern js changes from being available

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-typescript'],
+  presets: [ ['@babel/preset-env', {
+    targets: { node: '20' },
+    bugfixes: true,
+  }], '@babel/preset-typescript'],
   plugins: [
-    '@babel/plugin-transform-modules-commonjs',
     ['@babel/plugin-syntax-import-attributes', { deprecatedAssertSyntax: true }]
   ]
 };
